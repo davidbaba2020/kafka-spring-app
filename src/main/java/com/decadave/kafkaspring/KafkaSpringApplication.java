282e4ce6e9316ba6d1585f4ad9f1ada1c37bf30d
David Baba <davidbabasalifu2020.com>
2022-08-27 19:09:11 +0100

    Created my kafka topic configuration

--- src/main/java/com/decadave/kafkaspring/KafkaSpringApplication.java
+++ src/main/java/com/decadave/kafkaspring/KafkaSpringApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaSpringApplication {
+public class KafkaSpringApplication
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(KafkaSpringApplication.class, args);
 	}
 
