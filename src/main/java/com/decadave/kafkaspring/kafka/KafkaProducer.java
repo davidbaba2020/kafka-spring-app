ab83a7a5ada3dcb66ea6b3c7f0bc6f0c1582d162
David Baba <davidbabasalifu2020.com>
2022-08-30 01:37:44 +0100

    Created an API to send message to the kafkatopic

--- src/main/java/com/decadave/kafkaspring/kafka/KafkaProducer.java
+++ src/main/java/com/decadave/kafkaspring/kafka/KafkaProducer.java
@@ -2,6 +2,7 @@ package com.decadave.kafkaspring.kafka;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
@@ -15,6 +16,7 @@ public class KafkaProducer
     {
         this.kafkaTemplate = kafkaTemplate;
     }
+
 
 
     public void sendMessage(String message)
